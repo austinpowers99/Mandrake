877445b34cc9632090c351a63a76292cc332b7ee
austinpowers99 <samanthaf_9@outlook.com>
2023-08-22 15:10:30 -0700

    implement part of the categories API

---
+++ src/pages/PlantsCategoryPage/PlantsCategoryPage.jsx
@@ -0,0 +1,48 @@
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+
+export default function PlantsCategoryPage() {
+    const [categories, setCategories] = useState([]);
+
+    const API_KEY = process.env.REACT_APP_API_KEY;
+    const API_URL = 'https://perenual.com/api/';
+
+    useEffect(() => {
+        async function fetchCategories() {
+            try {
+                const response = await fetch(`${API_URL}plantscategory/`, {
+                    headers: {
+                        Authorization: `Token ${API_KEY}`,
+                    },
+                });
+                if (response.ok) {
+                    const data = await response.json();
+                    setCategories(data.results);
+                } else {
+                    throw new Error(`HTTP error! Status: ${response.status}`);
+                }
+            } catch (error) {
+                console.error('Error fetching categories:', error);
+            }
+        }
+        fetchCategories();
+    }, [API_KEY]);
+
+    return (
+        <>
+            <h1>Categories</h1>
+            <div className='category-container'>
+                {categories.map((category) => (
+                    <Link
+                        key={category.id}
+                        className='category-card-ink'
+                        to={`/exercise/${category.id}`}>
+                        <div>
+                            <h2>{category.name}</h2>
+                        </div>
+                    </Link>
+                ))}
+            </div>
+        </>
+    );
+}
