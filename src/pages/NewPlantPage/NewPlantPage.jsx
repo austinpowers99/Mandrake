f970ba0914820008d5b1d069adbfc9a94e7fd580
austinpowers99 <samanthaf_9@outlook.com>
2023-08-23 16:43:27 -0700

    create the form to upload a new plant

--- src/pages/NewPlantPage/NewPlantPage.jsx
+++ src/pages/NewPlantPage/NewPlantPage.jsx
@@ -1,7 +1,85 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+// Custom input component
+function InputField({ label, type, value, onChange }) {
+    return (
+        <label>
+            {label}:
+            <input
+            type={type}
+            value={value}
+            onChange={onChange}
+            required
+            />
+        </label>
+    );
+}
 
 export default function MyPlants() {
+    const [formData, setFormData] = useState({
+        commonName: '',
+        sunlight: '',
+        watering: '',
+        description: '',
+        growingTips: '',
+    });
+
+    const handleInputChange = (event) => {
+        const { name, value } = event.target;
+        setFormData({
+            ...formData,
+            [name]: value,
+        });
+    };
+
+    const handleSubmit = (evt) => {
+        evt.preventDefault();
+        // Add form submission logic here (formData)
+    }
+
     return (
-        <h1>Add New</h1>
+        <>
+            <h2 className="display-5" id="addNew">
+                What plants do you have?
+            </h2>
+            <form onSubmit={handleSubmit}>
+                <InputField
+                    label="Common Name"
+                    type="text"
+                    value={formData.plantCommonName}
+                    onChange={handleInputChange}
+                />
+                <br />
+                <InputField
+                    label="Sunlight"
+                    type="text"
+                    value={formData.plantSunlight}
+                    onChange={handleInputChange}
+                />
+                <br />
+                <InputField
+                    label="Watering"
+                    type="text"
+                    value={formData.plantWatering}
+                    onChange={handleInputChange}
+                />
+                <br />
+                <InputField
+                    label="Description"
+                    type="text"
+                    value={formData.plantDescription}
+                    onChange={handleInputChange}
+                />
+                <br />
+                <InputField
+                    label="Growing Tips"
+                    type="text"
+                    value={formData.plantGrowingTips}
+                    onChange={handleInputChange}
+                />
+                <br />
+                <button type="submit">Add Plant</button>
+            </form>
+        </>
     );
 }
