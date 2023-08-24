b769a5321f19e28ef66fd6324133e7cd9206f435
austinpowers99 <samanthaf_9@outlook.com>
2023-08-24 16:30:38 -0700

    update plants category using the API to find ideas by name

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -5,8 +5,8 @@ export default function HomePage() {
     <>
       <h1>Welcome to Mandrake!</h1>
       <p>
-      This is your passport to the wonderful world of plants and botanicals. Explore a vast variety of plant species from around the world. Dive into detailed descriptions, care guides, 
-      and growing tips for each plant, making it easy to care for your green companions! 
+      This is your passport to the wonderful world of plants and botanicals. Explore a vast variety of plant species from around the world. 
+      Dive into detailed descriptions, care guides, and growing tips for each plant, making it easy to care for your green companions! 
       </p>
       </>
   );
--- src/pages/PlantsCategoryPage/PlantsCategoryPage.jsx
+++ src/pages/PlantsCategoryPage/PlantsCategoryPage.jsx
@@ -1,10 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
-export default function PlantsCategoryPage() {
+export default function ExerciseCategoryPage() {
     const [categories, setCategories] = useState([]);
-    const [selectedCategories, setSelectedCategories] = useState([]); // To keep track of selected categories
-
+    
     const API_KEY = process.env.REACT_APP_API_KEY;
     const API_URL = `https://perenual.com/api/species-list?page=1&key=${API_KEY}`;
 
@@ -29,64 +28,16 @@ export default function PlantsCategoryPage() {
         fetchCategories();
     }, [API_KEY, API_URL]);
 
-    // Function to toggle the selection of a category
-    const handleCategoryToggle = (category) => {
-        if (selectedCategories.includes(category)) {
-            setSelectedCategories(selectedCategories.filter((c) => c !== category));
-        } else {
-            setSelectedCategories([...selectedCategories, category]);
-        }
-    };
-
-    // Function to check if a category is selected
-    const isCategorySelected = (category) => {
-        return selectedCategories.includes(category);
-    };
-
-    // Filter categories based on the selected categories
-    const filteredCategories = selectedCategories.length > 0
-        ? categories.filter((category) => selectedCategories.includes(category.sunlight))
-        : categories;
-
     return (
         <>
-            <h1>Categories</h1>
+            <h1>Find your green-half by name</h1>
             <div className='category-container'>
-                <div className='category-filters'>
-                    <button
-                        className={isCategorySelected('Name') ? 'active' : ''}
-                        onClick={() => handleCategoryToggle('Name')}>
-                        Name
-                    </button>
-                    <button
-                        className={isCategorySelected('Full Sun') ? 'active' : ''}
-                        onClick={() => handleCategoryToggle('Full Sun')}>
-                        Full Sun
-                    </button>
-                    <button
-                        className={isCategorySelected('Partial Shade') ? 'active' : ''}
-                        onClick={() => handleCategoryToggle('Partial Shade')}>
-                        Partial Shade
-                    </button>
-                    <button
-                        className={isCategorySelected('Frequent Watering') ? 'active' : ''}
-                        onClick={() => handleCategoryToggle('Frequent Watering')}>
-                        Frequent Watering
-                    </button>
-                    <button
-                        className={isCategorySelected('Average Watering') ? 'active' : ''}
-                        onClick={() => handleCategoryToggle('Average Watering')}>
-                        Average Watering
-                    </button>
-                </div>
-                {filteredCategories.map((category) => (
+                {categories.map((category) => (
                     <Link
                         key={category.id}
                         className='category-card-link'
                         to={`/categories/${category.id}`}>
                         <div>
-                            <h2>{category.sunlight}</h2>
-                            <h2>{category.watering}</h2>
                             <h2>{category.common_name}</h2>
                         </div>
                     </Link>
