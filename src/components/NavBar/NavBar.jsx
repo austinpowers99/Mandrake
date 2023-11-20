aa57599f67b04304db61c8f87bcf359195a30921
austinpowers99 <samanthaf_9@outlook.com>
2023-11-20 14:34:41 -0800

    add logo img to the nav bar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,7 +1,8 @@
+import React from 'react';
 import { Link } from 'react-router-dom';
 import * as userService from '../../utilities/users-service';
 import './NavBar.css';
-// import logo from '../../pages/logo.png';
+import logo from '../../pages/img/logo.png';
 
 export default function NavBar({ user, setUser }) {
   function handleLogOut() {
@@ -12,18 +13,16 @@ export default function NavBar({ user, setUser }) {
   return (
     <nav>
       <div className='nav'>
-      {/* <div className="logo">
-        <img src={logo} alt="Mandrake Logo" className="logo" />
-      </div> */}
-        {/* &nbsp; &nbsp; */}
+        {/* <div className='logo'> */}
+          <img src={logo} alt="Mandrake Logo" className="nav-logo" />
+        {/* </div> */}
+
         <Link to="/">Home</Link>
-        {/* &nbsp; &nbsp; */}
         <Link to="/categories">Categories</Link>
-        {/* &nbsp; &nbsp; */}
         {user ? (
           <>
             <Link to="/plants">My Plants</Link>
-            {/* &nbsp; &nbsp; */}
+
             <Link to="/plants/new">Add New</Link>
             &nbsp; &nbsp;<Link to="" onClick={handleLogOut}>Log Out</Link>
           </>
